4f4406c90d32d2fd3bb49c91eb7864cf84cc54e9
Viet <VietTL>
2024-11-25 14:26:12 +0700

    update project

--- Web/react-learn/react-essentials/src/components/TabButton.jsx
+++ Web/react-learn/react-essentials/src/components/TabButton.jsx
@@ -2,7 +2,6 @@
 export default function TabButton(
     { children, onSelect, isSelected }
 ) {
-    console.log('TABBUTTON COMPONENT EXECUTING');
     return(
         <li>
             <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
